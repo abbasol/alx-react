6038cd1bdb9ce79a8a224295eaee8f073f5dc8df
abbasol <abbas_fu@outlook.sa>
2024-07-26 20:33:48 -0400

    done

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,5 @@
+/* full-year */
+export const getFullYear = () => new Date().getFullYear();
+
+/* footer-copy */
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
