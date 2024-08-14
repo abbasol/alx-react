d760d6348e2ac62230209c61c12108b0dcef04ed
abbasol <abbas_fu@outlook.sa>
2024-08-13 21:13:12 -0400

    good

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,23 +1,15 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(object) {
-  const seq = Seq(object);
+export default function printBestStudents(grades) {
+	const bestStudents = Seq(grades)
+		.filter((student) => student.score >= 70)
+		.map((student) => ({
+			score: x.score,
+			firstName:
+				student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+			lastName:
+				student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+		}));
 
-  const chosen = seq.filter((student) => {
-  student.firstName.charAt(0).toUpperCase();
-  return student.score > 70;
-  });
-
-  function capFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  const OurObject = chosen.toJS();
-
-  Object.keys(OurObject).map((key) => {
-  OurObject[key].firstName = capFirstLetter(OurObject[key].firstName);
-  OurObject[key].lastName = capFirstLetter(OurObject[key].lastName);
-  return OurObject[key];
-  });
-  console.log(OurObject);
-  }
+	console.log(bestStudents.toObject());
+}
